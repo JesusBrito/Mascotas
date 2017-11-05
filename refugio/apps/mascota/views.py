f7ca2e3db1b8c8dfcd10074db635b030c0c25bb7
JesusBrito <jesus291196@gmail.com>
2017-11-04 18:55:30 -0600

    Finalizando las Vistas basadas en clases de todo el CRUD :3 estoy empezando a amar Django

--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from apps.mascota.forms import MascotaForm
 from apps.mascota.models import Mascota
@@ -45,6 +45,7 @@ def mascota_delete(request, id_mascota):
 		return redirect('mascota:mascota_listar')
 	return render(request,'mascota/mascota_delete.html',{'mascota':mascota})
 
+##Vistas basadas en clases
 class MascotaList(ListView):
 	model = Mascota
 	template_name ='mascota/mascota_list.html'
@@ -54,4 +55,14 @@ class MascotaCreate(CreateView):
 	form_class = MascotaForm
 	template_name='mascota/mascota_form.html'
 	success_url = reverse_lazy('mascota:mascota_listar')
-						
+
+class MascotaUpdate(UpdateView):
+	model= Mascota 
+	form_class = MascotaForm
+	template_name='mascota/mascota_form.html'
+	success_url = reverse_lazy('mascota:mascota_listar')
+
+class MascotaDelete(DeleteView):
+	model= Mascota
+	template_name='mascota/mascota_delete.html'
+	success_url = reverse_lazy('mascota:mascota_listar')					
