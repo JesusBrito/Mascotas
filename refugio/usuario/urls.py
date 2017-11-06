61af632f07f0b4b4e5a333b58080a368d5d4bd09
JesusBrito <jesus291196@gmail.com>
2017-11-05 19:14:12 -0600

    Agregando registros de usuario

--- refugio/usuario/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-
-from apps.usuario.views import RegistroUsuario
-
-urlpatterns = [
-    url(r'^registrar$', RegistrarUsuario.as_view(),  name ='usuario_crear'),
-]
