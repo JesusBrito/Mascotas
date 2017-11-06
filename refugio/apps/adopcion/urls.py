4162588521e675dff2b1a100620a85ebf460d92b
JesusBrito <jesus291196@gmail.com>
2017-11-05 22:27:21 -0600

    Envio de correo para el reestablecimiento de las contrasenias de usuario y bloqueo de URLS para usuarios no registrados

--- refugio/apps/adopcion/urls.py
+++ refugio/apps/adopcion/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
 from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate,SolicitudUpdate,SolicitudDelete
 
 urlpatterns = [
 	url(r'^index$',index_adopcion),
-	url(r'^listar$',SolicitudList.as_view(), name='solicitud_listar'),
-	url(r'^nueva$',SolicitudCreate.as_view(), name='solicitud_crear'),
-	url(r'^editar/(?P<pk>\d+)/$',SolicitudUpdate.as_view(), name='solicitud_editar'),
-	url(r'^eliminar/(?P<pk>\d+)/$',SolicitudDelete.as_view(), name='solicitud_eliminar'),
+	url(r'^listar$',login_required(SolicitudList.as_view()), name='solicitud_listar'),
+	url(r'^nueva$',login_required(SolicitudCreate.as_view()), name='solicitud_crear'),
+	url(r'^editar/(?P<pk>\d+)/$',login_required(SolicitudUpdate.as_view()), name='solicitud_editar'),
+	url(r'^eliminar/(?P<pk>\d+)/$',login_required(SolicitudDelete.as_view()), name='solicitud_eliminar'),
 ]
